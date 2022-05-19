cf40df71cbb7c0fc60776cf653d219cc121d193a
d1g009 <dharmikgodhani071@gmail.com>
2022-05-19 12:37:23 +0530

    issue solve

--- src/app/dashboard/statistics-card/statistics-card.model.spec.ts
+++
@@ -1,7 +0,0 @@
-import { StatisticsCard } from './statistics-card.model';
-
-describe('StatisticsCard', () => {
-  it('should create an instance', () => {
-    expect(new StatisticsCard()).toBeTruthy();
-  });
-});
